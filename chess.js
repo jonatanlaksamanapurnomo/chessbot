beceb970e3a925b4595cec047d1fdfa8b43ac367
Joo <jonatanlaksamanapurnomo>
2020-11-14 18:10:39 +0700

    add git ignore

---
+++ chess.js
@@ -0,0 +1,86 @@
+let domBoard = document.getElementsByClassName("board")[0];
+let domClock = document.getElementsByClassName("clock-bottom")[0];
+
+function isMyTurn() {
+    return domClock.classList.contains('clock-player-turn');
+}
+
+function side() {
+    return domBoard.classList.contains('flipped') ? "b" : "w";
+}
+
+function injected() {
+    return "injected woi";
+}
+
+function boardToFEN(board) {
+    let FENstr = "";
+    let emptyCounter = 0;
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            let curPiece = board[7 - i][j];
+            if (curPiece != "") {
+                if (emptyCounter > 0) {
+                    FENstr += emptyCounter.toString();
+                    emptyCounter = 0;
+                }
+                FENstr += curPiece;
+            } else {
+                emptyCounter += 1;
+            }
+        }
+        if (emptyCounter > 0) {
+            FENstr += emptyCounter.toString();
+            emptyCounter = 0;
+        }
+        FENstr += "/";
+    }
+    return FENstr.substr(0, FENstr.length - 1) + " " + side();
+}
+
+function getBoard() {
+    let pieces = Array.from(document.getElementsByClassName("piece"));
+    let board = [...new Array(8)].map(elem => new Array(8).fill(""));
+    for (let i = 0; i < pieces.length; i++) {
+        let pieceType = pieces[i].getAttribute("class").split(" ")[1];
+        pieceType = pieceType[0] == "w" ? pieceType[1].toUpperCase() : pieceType[1];
+        let chessComPos = pieces[i].getAttribute("class").split("square-")[1].split("").map((x) => parseInt(x) - 1);
+        // board[row][col]
+        //if (boardFlipped) board[7 - chessComPos[1]][7 - chessComPos[0]] = pieceType;
+        //else board[chessComPos[1]][chessComPos[0]] = pieceType;
+        board[chessComPos[1]][chessComPos[0]] = pieceType;
+    }
+    return board;
+}
+
+function httpPostAsync(theUrl, fen, callback) {
+    let xmlHttp = new XMLHttpRequest();
+
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) callback(xmlHttp.responseText);
+    }
+
+    xmlHttp.open("POST", theUrl, true); // true for asynchronous
+    xmlHttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+    let bodyParams = {
+        "fenString": fen
+    }
+    xmlHttp.send(JSON.stringify(bodyParams));
+}
+
+function getBoardMakeGuess() {
+    if (isMyTurn()) {
+        let fenQuery = boardToFEN(getBoard());
+        httpPostAsync("http://localhost:3000/bestmove", fenQuery, (move) => {
+            console.log(move);
+        });
+    }
+}
+
+playerTurnChange = function (changes) {
+    setTimeout(getBoardMakeGuess, 750);
+};
+let observer = new MutationObserver(playerTurnChange);
+observer.observe(domClock, {
+    attributes: true
+});
---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = 3000
+let cors = require('cors')
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true})) // for parsing application/x-www-form-urlencoded
+app.use(cors())
+
+
+let STOCKFISH = require("stockfish");
+let stockfish = STOCKFISH();
+
+function makeMove(fen) {
+    stockfish.postMessage("position fen " + fen);
+    stockfish.postMessage("go depth 15");
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello World!@')
+})
+
+// fenString : "rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w"
+app.post('/bestmove/', (req, res) => {
+    let fenString = req.body.fenString;
+    Promise.resolve(makeMove(fenString)).then(() => {
+        stockfish.onmessage = (event) => {
+            if (event && event.includes("bestmove")) {
+                let bestMove = event.split(' ')[1];
+                res.send(bestMove)
+            }
+        };
+
+    });
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
